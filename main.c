ea46c4485772a48db12121d05cab5c1b1360f494
3670460 <paul-antoine.levesque@etu.upmc.fr>
2019-03-21 10:28:48 +0100

    update

--- main.c
+++ main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
+#include <time.h>
 #include "functions.h"
 
-int grille[9][9] = {
-  {0,0,9,7,0,8,3,0,0},
-  {0,2,0,6,0,0,0,4,0},
-  {0,0,0,0,0,9,0,0,7},
-  {0,0,8,4,0,5,6,9,0},
-  {0,6,0,0,0,0,0,0,1},
-  {0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,8,0,0,0,0},
-  {0,9,0,0,2,0,4,0,3},
-  {0,7,4,5,6,0,0,0,0},
+int grille[9][9] = { //mise en forme selon la grille de sudokua
+  {1, 0, 0,    0, 0, 7,    0, 9, 0},
+  {0, 3, 0,    0, 2, 0,    0, 0 ,8},
+  {0, 0, 9,    6, 0, 0,    5, 0 ,0},
+
+  {0, 0, 5,    3, 0, 0,    9, 0, 0},
+  {0, 1, 0,    0, 8, 0,    0, 0, 2},
+  {6, 0, 0,    0, 0, 4,    0, 0, 0},
+
+  {3, 0, 0,    0, 0, 0,    0, 1, 0},
+  {0, 4, 0,    0, 0, 0,    0, 0, 7},
+  {0, 0, 7,    0, 0, 0,    3, 0, 0},
 };
 
 int main(){
   printf("Grille non resolue : \n");
   afficher_jeu(grille);
+  clock_t t1, t2;
+  float temps;
+  t1 = clock();
   if(resoudre2(grille, 0)){
-	printf("Grille resolue : \n");
-	afficher_jeu(grille);
+    printf("Grille resolue : \n");
+    afficher_jeu(grille);
   }
   else{
-	printf("Erreur\n");
+    printf("Erreur\n");
   }
+  t2 = clock();
+  temps = (float) (t2-t1)/CLOCKS_PER_SEC; //calcul du temps d'execution
+  printf("Temps d'execution : %f secondes.\n", temps);
   return 0;
 }
 
-	  
